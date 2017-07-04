cbe99c60a20fd600c5a97939d117239c115233ac
davidfeng88 <david.feng.ge@gmail.com>
2017-07-04 10:44:56 -0400

    finish phase 1f

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -3,8 +3,19 @@ require 'active_support/inflector'
 
 class SQLObject
 
-  def self.columns
+  # class methods
 
+  def self.table_name
+    @table_name ||= self.to_s.tableize
+    # e.g. self is the Dog class. self.to_s and self.name both give "Dog" String
+    # tableize = underscore * pluralize
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name
+  end
+
+  def self.columns # it returns an array of column names (symbols) in a table
     if @columns.nil?
       info = DBConnection.execute2(<<-SQL)
         SELECT
@@ -13,32 +24,22 @@ class SQLObject
           #{self.table_name}
       SQL
       @columns = info[0].map(&:to_sym)
+      # execute2 returns an array, the first element is an array of
+      # column names (Strings)
+      # SQL will only let you use ? to interpolate values,
+      # not table or column names (e.g. in the FROM statement)
     end
-
     @columns
-    # solution: LIMIT 0
-    # It can also be employed to obtain the types of the result columns
-    # if you are using a MySQL API that makes result set metadata available.
   end
 
   def self.finalize!
-    columns.each do |col|
-      define_method(col.to_s) { attributes[col] }
-      define_method("#{col.to_s}=") { |val| attributes[col] = val }
+    # Finalize is called at the end of the subclass definition to
+    # add the getters/setters.
+    self.columns.each do |col|
+      define_method(col.to_s) { self.attributes[col] }
+      define_method("#{col}=") { |val| self.attributes[col] = val }
+      # "#{:name}" == "name" String interpolation convert symbol to string
     end
-  end
-  # "#{:name}" == "name"
-
-  def self.table_name=(table_name)
-    @table_name = table_name
-  end
-
-  def self.table_name
-    @table_name ||= self.to_s.tableize
-    # e.g. self is the Dog class. self.to_s and self.name both give "Dog" String
-    # tableize = underscore * pluralize
-    # @table_name = self.to_s.tableize if @table_name.nil?
-    @table_name
   end
 
   def self.all
@@ -48,23 +49,9 @@ class SQLObject
       FROM
         #{self.table_name}
     SQL
-    @objects_array = self.parse_all(results)
-
-  end
-
-  def self.parse_all(results)
-    array = []
-    results.each do |row|
-      el = self.new
-      columns.each do |col|
-        el.send("#{col.to_s}=", row[col])
-      end
-      array << el
-    end
-    # solution:
-    # results.map{ |result| self.new(result) }
-
-    array
+    self.parse_all(results)
+    # convert the array of Hash objects to an array of specific object
+    # e.g. Cat object.
   end
 
   def self.find(id)
@@ -76,16 +63,21 @@ class SQLObject
       WHERE
         #{self.table_name}.id = ?
     SQL
-    return nil if results.length == 0
-    self.new(results[0])
+    self.parse_all(results).first
   end
 
+  # instance methods
+  
   def initialize(params = {})
     params.each do |attr_name, val|
-      unless self.class.columns.include?(attr_name.to_sym)
+      attr_name = attr_name.to_sym
+      if self.class.columns.include?(attr_name)
+        # call ::columns on a class object, not the instance
+        self.send("#{attr_name}=", val)
+        # avoid using @attributes or #attributes inside #initialize.
+      else
         raise "unknown attribute '#{attr_name}'"
       end
-      self.send("#{attr_name.to_sym}=", val)
     end
   end
 
@@ -128,12 +120,13 @@ class SQLObject
   end
 
   def save
-    if id.nil?
-      insert
-    else
-      update
-    end
-    # solution
-    # id.nil? ? insert : update
+    id.nil? ? insert : update
   end
+
+  private
+
+  def self.parse_all(results)
+    results.map{ |result| self.new(result) }
+  end
+
 end
