c01b635ce236be90714549dd6c29ecc41f307d71
davidfeng88 <david.feng.ge@gmail.com>
2017-07-04 11:42:12 -0400

    finish phase 2

--- lib/02_searchable.rb
+++ lib/02_searchable.rb
@@ -6,28 +6,19 @@ module Searchable
     where_line = params.keys.map {|key| "#{key} = ?"}.join(" AND ")
     vals = params.values
 
-    results_as_array_of_hashes = DBConnection.execute(<<-SQL, *vals)
+    results = DBConnection.execute(<<-SQL, *vals)
       SELECT
         *
       FROM
-        #{table_name}
+        #{self.table_name}
       WHERE
         #{where_line}
     SQL
 
-    # solution:
-    # parse_all(results)
-
-    results_as_array_of_objects = []
-    results_as_array_of_hashes.each do |hash|
-      new_obj = self.new(hash)
-      results_as_array_of_objects << new_obj
-    end
-    results_as_array_of_objects
+    parse_all(results)
   end
 end
 
 class SQLObject
-  # Mixin Searchable here...
   extend Searchable
 end
