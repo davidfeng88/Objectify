64ca02ff25a85f41a7f016d43d78bdd75c4ee45a
davidfeng88 <david.feng.ge@gmail.com>
2017-07-04 10:58:57 -0400

    finish phase 1

--- lib/01_sql_object.rb
+++ lib/01_sql_object.rb
@@ -6,28 +6,29 @@ class SQLObject
   # class methods
 
   def self.table_name
-    @table_name ||= self.to_s.tableize
     # e.g. self is the Dog class. self.to_s and self.name both give "Dog" String
     # tableize = underscore * pluralize
+    @table_name ||= self.to_s.tableize
   end
 
   def self.table_name=(table_name)
     @table_name = table_name
   end
 
-  def self.columns # it returns an array of column names (symbols) in a table
+  def self.columns
+    # it returns an array of column names (symbols) in a table
     if @columns.nil?
-      info = DBConnection.execute2(<<-SQL)
-        SELECT
-          *
-        FROM
-          #{self.table_name}
-      SQL
-      @columns = info[0].map(&:to_sym)
       # execute2 returns an array, the first element is an array of
       # column names (Strings)
       # SQL will only let you use ? to interpolate values,
       # not table or column names (e.g. in the FROM statement)
+      info = DBConnection.execute2(<<-SQL)
+      SELECT
+      *
+      FROM
+      #{self.table_name}
+      SQL
+      @columns = info[0].map(&:to_sym)
     end
     @columns
   end
@@ -37,8 +38,8 @@ class SQLObject
     # add the getters/setters.
     self.columns.each do |col|
       define_method(col.to_s) { self.attributes[col] }
-      define_method("#{col}=") { |val| self.attributes[col] = val }
       # "#{:name}" == "name" String interpolation convert symbol to string
+      define_method("#{col}=") { |val| self.attributes[col] = val }
     end
   end
 
@@ -49,9 +50,9 @@ class SQLObject
       FROM
         #{self.table_name}
     SQL
-    self.parse_all(results)
     # convert the array of Hash objects to an array of specific object
     # e.g. Cat object.
+    self.parse_all(results)
   end
 
   def self.find(id)
@@ -67,14 +68,14 @@ class SQLObject
   end
 
   # instance methods
-  
+
   def initialize(params = {})
     params.each do |attr_name, val|
       attr_name = attr_name.to_sym
+      # call ::columns on a class object, not the instance
       if self.class.columns.include?(attr_name)
-        # call ::columns on a class object, not the instance
-        self.send("#{attr_name}=", val)
         # avoid using @attributes or #attributes inside #initialize.
+        self.send("#{attr_name}=", val)
       else
         raise "unknown attribute '#{attr_name}'"
       end
@@ -87,13 +88,15 @@ class SQLObject
 
   def attribute_values
     # @attributes.values
-    self.class.columns.map {|el| self.send(el) }
+    self.class.columns.map {|attr| self.send(attr) }
   end
 
   def insert
-    col_names = self.class.columns[1..-1].map(&:to_s).join(", ")
-    question_marks = (["?"] * self.class.columns[1..-1].count).join(", ")
-    no_id_attr_array = attribute_values[1..-1]
+    # drop the first column to avoid inserting id
+    columns = self.class.columns.drop(1)
+    col_names = columns.map(&:to_s).join(", ")
+    question_marks = (["?"] * columns.count).join(", ")
+    no_id_attr_array = attribute_values.drop(1)
 
     DBConnection.execute(<<-SQL, *no_id_attr_array)
       INSERT INTO
@@ -105,7 +108,8 @@ class SQLObject
   end
 
   def update
-    set_line = self.class.columns[1..-1].map{ |attr_name| "#{attr_name} = ? "}.join(", ")
+    set_line = self.class.columns[1..-1]
+      .map{ |attr_name| "#{attr_name} = ? "}.join(", ")
 
     id_at_last_array = attribute_values.rotate
 
